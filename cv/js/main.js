9f34124cde4b91d6aab1abfc60e65100236ce55e
jingjingshi <1069072344@qq.com>
2017-07-28 10:09:18 +0800

    gaibian

---
+++ cv/js/main.js
@@ -0,0 +1,19 @@
+ $(document).ready(function() {
+$('#fullpage').fullpage({
+	//设置背景颜色
+	sectionsColor:['#FFCE44','#7DD0F4','#A2F3A4','#F54BDB'],
+	//设置锚链接
+	anchors:['page1','page2','page3','page4'],
+	//设置固定元素
+	fixedElements:'#header',
+	//设置翻页小圆点
+	navigation:true,
+	navigationPosition:'right',
+	navigationTooltips:['个人简介','掌握技能','我的作品','关于我'],
+	//翻页小圆点的显示
+    showActiveTooltip:true,
+    //满屏有滚动条，需插件配合，scrollOverflow
+    //自己控制页面样式
+    //verticalCentered:false
+});
+});
